caa42be1c0a20dd53894696564ebe7e584e49df6
shin_seong_cheol <cher3725@naver.com>
2023-10-24 23:27:30 +0900

    production 설정 파일 수정

--- config/production.py
+++ config/production.py
@@ -1,5 +1,5 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR), 'pybo.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_MODIFICATIONS = False
 SECRET_KEY = b'\x84\xdf\xb5\x13\xac\xfd\x8f[\xc2)<\xc9\xfb\xd8N\xb3'
--- run.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set FLASK_APP=pybo
-set FLASK_DEBUG=true
-E:\dev\python\project\myproject\venv\Scripts\activate
