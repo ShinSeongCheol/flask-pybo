f785fb30894a9f48e1ba8bdfce637eafd9ce1915
Shin_Seong_Cheol <cher3725@naver.com>
2023-10-24 23:51:23 +0900

    404 오류 등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -14,6 +14,8 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
     app = Flask(__name__)
@@ -38,5 +40,8 @@ def create_app():
     from .filter import format_datetime
 
     app.jinja_env.filters["datetime"] = format_datetime
+
+    # 오류 핸들러
+    app.register_error_handler(404, page_not_found)
 
     return app
