1168c7642b9b1e3db67a3facd628f6e17f6d2106
Shin_Seong_Cheol <cher3725@naver.com>
2023-10-25 20:55:38 +0900

    로깅 설정 추가

--- config/production.py
+++ config/production.py
@@ -1,5 +1,28 @@
 from config.default import *
+from logging.config import dictConfig
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(BASE_DIR, "pybo.db"))
 SQLALCHEMY_MODIFICATIONS = False
-SECRET_KEY = b'\x84\xdf\xb5\x13\xac\xfd\x8f[\xc2)<\xc9\xfb\xd8N\xb3'
+SECRET_KEY = b"\x84\xdf\xb5\x13\xac\xfd\x8f[\xc2)<\xc9\xfb\xd8N\xb3"
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
+            }
+        },
+        "handler": {
+            "file": {
+                "level": "INFO",
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": os.path.join(BASE_DIR, "logs/myproject.log"),
+                "maxBytes": 1024 * 1024 * 5,
+                "backupCount": 5,
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["file"]},
+    },
+)
